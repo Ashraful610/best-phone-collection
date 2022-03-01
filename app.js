eefcaad07357452fe012c81f42cdfc54347e9554
AshrafulAlam <ashalam610@gmail.com>
2022-03-01 18:30:39 +0600

    forth-commit

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 const loadData = () => {
    const field = document.getElementById('search-field') 
    const fieldValue = field.value
-   document.getElementById('spinner-div').style.display = 'block'
-  const url = ` https://openapi.programming-hero.com/api/phones?search=${fieldValue}`
-    fetch(url)
-     .then(res => res.json())
-     .then(datas => displayData(datas.data))
+   if(fieldValue === ''){
+      document.getElementById('errorMessage1').style.display='block'
+   }
+   else{
+      document.getElementById('errorMessage1').style.display='none'
+      const url = ` https://openapi.programming-hero.com/api/phones?search=${fieldValue}`
+      fetch(url)
+       .then(res => res.json())
+       .then(datas => displayData(datas.data))
 
+   }
+     document.getElementById('spinner-div').style.display = 'block'
      // field clean 
      field.value = ''
 
@@ -15,18 +21,18 @@ const loadData = () => {
 
 // display phones 
 const displayData = phones => {
-    // console.log(phones)
+   //  console.log(phones)
     const resultsArea = document.getElementById('results-area')
       resultsArea.textContent = ''
       const detailsArea = document.getElementById('detalis-area')
       detailsArea.textContent = ''
-    phones.forEach(phone => {
+    phones?.forEach(phone => {
           const div = document.createElement('div')
           div.classList.add('col-lg-4')
           div.classList.add('p-2')
           div.classList.add('mb-2')
             div.innerHTML = `
-               <div class="card">
+               <div class="card card1">
                         <img src="${phone.image}" id="phone-img" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h4 class="card-title"> Name :  ${phone.phone_name}</h4>
@@ -44,6 +50,7 @@ const displayData = phones => {
 // details button
 const detailsButton = detail => {
     const url = `  https://openapi.programming-hero.com/api/phone/${detail}`
+    console.log(url)
     fetch(url)
      .then(res => res.json())
      .then(datas => loadDetails(datas.data))
@@ -51,8 +58,9 @@ const detailsButton = detail => {
 
 // phone Details
  const loadDetails = phones => {
-    //  console.log(phones)
+   //   console.log(phones)
      const detailsArea = document.getElementById('detalis-area')
+      detailsArea.textContent=''
      const div = document.createElement('div')
      div.classList.add('col-lg-4')
      div.classList.add('p-2')
@@ -62,7 +70,7 @@ const detailsButton = detail => {
                         <img src="${phones.image}" id="phone-img" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title"> Name : ${phones.name}</h5>
-                        <h5 class="card-title">  Release Date : ${phones.releaseDate}</h5>
+                        <h5 class="card-title">  Release Date : ${phones.releaseDate?phones.releaseDate:'Release Date not found'}</h5>
                         <h5 class="fw-bold">Main Feature :</h5>
                         
                         <h6>RAM: ${phones.mainFeatures.memory.substr(6,4)}</h6>
@@ -70,11 +78,12 @@ const detailsButton = detail => {
                         <h6>Display Size: ${phones.mainFeatures.displaySize.slice(0,10)}</h6>
 
                         <h5 class="fw-bold"> Other Feature :</h5>
-                        <h6> Bluetooth: ${phones.others.Bluetooth}</h6>
-                        <h6>GPS: ${phones.others.GPS.slice(0,15)}</h6>
-                        <h6>USB: ${phones.others.USB}</h6>
-                        <h6>Radio: ${phones.others.Radio}</h6>
-                        <h6>WLAN: ${phones.others.WLAN}</h6>
+                        <h6> Bluetooth: ${phones.others?.Bluetooth? phones.others.Bluetooth:'No'}</h6>
+                        <h6>GPS: ${phones.others?.GPS.slice(0,15)?phones.others.GPS:'No'}</h6>
+                        <h6>USB: ${phones.others?.USB?phones.others.USB:'No'}</h6>
+                        <h6>NFC: ${phones.others?.NFC?phones.others.NFC:'No'}</h6>
+                        <h6>Radio: ${phones.others?.Radio?phones.others.Radio:'No'}</h6>
+                        <h6>WLAN: ${phones.others?.WLAN?phones.others.WLAN:'No'}</h6>
                  
                      </div>
                </div>
